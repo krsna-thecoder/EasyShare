336e42cce23377c605c0d7bc8aeb350b62718e36
krsna-thecoder <krishna1>
2026-06-13 14:17:09 +0530

    Fix github pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import LandingPage from './pages/LandingPage.jsx'
 import WorkspacePage from './pages/WorkspacePage.jsx'
@@ -8,12 +8,12 @@ import ShareCreatedPage from './pages/ShareCreatedPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/workspace/:accessCode" element={<WorkspacePage />} />
         <Route path="/share-created/:accessCode" element={<ShareCreatedPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
